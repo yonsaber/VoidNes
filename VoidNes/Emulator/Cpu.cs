04ed266775a6a4aa3ff551bbfdaa46e9b679290d
Riley Redmond <riley@hidinginthevoid.com>
2022-09-19 22:57:59 +0100

    Start of handling opcodes!

--- VoidNes/Emulator/Cpu.cs
+++ VoidNes/Emulator/Cpu.cs
@@ -15,6 +15,8 @@ public class Cpu
     public byte S; // Stack Pointer
     public ushort PC; // Program Counter
 
+    public ulong Clock;
+
     // Flags
     bool _carry;
     bool _zero;
@@ -38,11 +40,38 @@ public class Cpu
         X = 0;
         Y = 0;
         S = 0xFD;
-        PC = 0; // Not exactly correct
 
         System = system;
+
+        Clock = 0;
 
         var resetVector = System.ReadWord(0xFFC);
         PC = resetVector;
     }
+
+    #region CPU Instruction Set
+
+    // LoaD Accumulator -- retrieves a copy from the specified RAM or I/O address, and stores it in the accumulator
+    public void lda(byte opcode)
+    {
+        // TODO: Possibly look into doing a yield return
+        byte intermediate = 0;
+        switch (opcode)
+        {
+            case 0xa9:
+                Clock += 2;
+                intermediate = System.ReadByte((ushort)(PC + 1));
+                break;
+            default:
+                Console.WriteLine($"Unknown opcode {opcode}");
+                break;
+        }
+
+        _negative = (intermediate & 0x80) == 0x80;
+        _zero = intermediate == 0;
+
+        A = intermediate;
+    }
+
+    #endregion
 }
